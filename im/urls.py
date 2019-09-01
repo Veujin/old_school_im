be82a68e4c2aaeef593db6285cf7e5a444e56538
Mikhail Bolshakov <veujin@Mikhails-MacBook-Pro.local>
2019-09-01 18:52:39 +0300

    Added redirect from root and loging_required on both views

--- im/urls.py
+++ im/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 app_name = 'im'
 urlpatterns = [
-    path('chats', views.ChatsView.as_view(), name='chat-list'),
-    path('chats/<int:chat_id>', views.ChatMessagesView.as_view(), name='chat')
+    path('chats', login_required(views.ChatsView.as_view()), name='chat-list'),
+    path('chats/<int:chat_id>', login_required(views.ChatMessagesView.as_view()), name='chat')
 ]
